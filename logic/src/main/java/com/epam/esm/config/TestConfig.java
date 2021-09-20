4338cdee9f3747d45fb75557299aab1dfc33645f
Nicky-Nik-Nik <nickich1789@gmail.com>
2021-09-20 17:15:08 +0300

    security

--- logic/src/main/java/com/epam/esm/config/TestConfig.java
+++ logic/src/main/java/com/epam/esm/config/TestConfig.java
@@ -2,18 +2,26 @@ package com.epam.esm.config;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.esm")
 @Profile("test")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.epam.esm")
 @PropertySource("classpath:application-test.properties")
 public class TestConfig {
     private static final Logger logger = LogManager.getLogger();
@@ -29,11 +37,16 @@ public class TestConfig {
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
+    public LocalSessionFactoryBean entityManagerFactory(DataSource dataSource){
         LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
         factory.setDataSource(dataSource);
         factory.setPackagesToScan(PACKAGES_TO_SCAN);
         return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(@Autowired SessionFactory sessionFactory) {
+        return new JpaTransactionManager(sessionFactory);
     }
 
     @Bean
